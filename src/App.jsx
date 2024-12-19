8d80e16545c2a90a07d6047080cf23e295844067
MDBBee <bobbyugbebor@gmail.com>
2024-12-19 09:43:42 +0200

    Project ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './app.scss';
+import Contact from './components/contact/Contact';
+import Cursor from './components/cursor/Cursor';
+
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
 import Sidebar from './components/sidebar/Sidebar';
 import Test from './Test';
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <Sidebar />
       <section id="Homepage">
         <Navbar />
@@ -16,15 +22,16 @@ const App = () => {
       <section id="Services">
         <Parallax type="services" />
       </section>
-      <section>Services</section>
+      <section>
+        <Services />
+      </section>
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section id="Contact">Contact</section>
-      {/* <Test /> */}
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
